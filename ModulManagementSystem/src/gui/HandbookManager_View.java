24a43d3c2244d9fcab3e239b12ffc0d0783a42d0
abca <manuel.bauer@uni-ulm.de>
2013-07-09 11:33:38 +0200

    ich tus

--- ModulManagementSystem/src/gui/HandbookManager_View.java
+++ ModulManagementSystem/src/gui/HandbookManager_View.java
@@ -1,9 +1,11 @@
 package gui;
 
+import java.net.URL;
 import java.util.LinkedList;
 
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
@@ -17,6 +19,8 @@ import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Window.Notification;
+import com.vaadin.ui.themes.BaseTheme;
+import com.vaadin.ui.themes.Runo;
 
 import control.Controller;
 import control.ModuleTable;
@@ -26,7 +30,14 @@ import control.ModuleTree;
 import data.BookData;
 import data.ModulDatabase;
 
-public class HandbookManager_View extends Startseite {
+public class HandbookManager_View extends Startseite implements Button.ClickListener{
+	
+	private VerticalLayout vl;
+	private Panel pan;
+	private AbsoluteLayout mainLayout;
+	private Label label;
+	private Button back;
+	private URL oldURL;
 	
 	public Window w;
 	public Panel p;
@@ -65,59 +76,197 @@ public class HandbookManager_View extends Startseite {
 		starta.addWindow(w);
 		
 		moduletree = mt.generateModuleTree(id);
-		w.addComponent(moduletree);
-		w.addComponent(new Label("-"));
+		//w.addComponent(moduletree);
+		//w.addComponent(new Label("-"));
 		choice = new Label("Ausgewähltes Element: -");
-		w.addComponent(choice);
+		//w.addComponent(choice);
 		
-		p = new Panel();
-		p.setContent(new HorizontalLayout());
+		//p = new Panel();
+		//p.setContent(new HorizontalLayout());
 		fachname = new TextField();
 		fachname.setEnabled(false);
-		p.addComponent(fachname);
+		//p.addComponent(fachname);
 		addFach = new Button("Neues Fach");
 		addFach.setEnabled(false);
-		p.addComponent(addFach);
+		//p.addComponent(addFach);
 		delete = new Button("Löschen");
-		p.addComponent(delete);
-		w.addComponent(p);
-		w.addComponent(new Label("-"));
+		//p.addComponent(delete);
+		//w.addComponent(p);
+		//w.addComponent(new Label("-"));
 		
-		tablePanel1 = new Panel();
+		/*tablePanel1 = new Panel();
 		tablePanel1.setContent(new HorizontalLayout());
 		tablePanel11 = new Panel();
-		tablePanel11.setContent(new VerticalLayout());
+		tablePanel11.setContent(new VerticalLayout());*/
 		
 		umta = new UnassignedModulesTable(cont.getUserID());
 		unassignedModules = umta.generateTable();
-		tablePanel11.addComponent(unassignedModules);
+		//tablePanel11.addComponent(unassignedModules);
 		
 		add1 = new Button("Modul dem ausgewählten Fach hinzufügen");
 		add1.setEnabled(false);
-		tablePanel11.addComponent(add1);
-		tablePanel1.addComponent(tablePanel11);
+		//tablePanel11.addComponent(add1);
+		//tablePanel1.addComponent(tablePanel11);
 
-		tablePanel12 = new Panel();
-		tablePanel12.setContent(new VerticalLayout());
+		//tablePanel12 = new Panel();
+		//tablePanel12.setContent(new VerticalLayout());
 		
 		mta = new ModuleTable(cont.getUserID());
 		System.out.println("User-ID:" + cont.getUserID());
 		modules = mta.generateTable();
-		tablePanel12.addComponent(modules);
+		//tablePanel12.addComponent(modules);
 		
 		add2 = new Button("Modul dem ausgewählten Fach hinzufügen");
 		add2.setEnabled(false);
-		tablePanel12.addComponent(add2);
+		//tablePanel12.addComponent(add2);
 		
-		tablePanel1.addComponent(tablePanel12);
-		w.addComponent(tablePanel1);
+		//tablePanel1.addComponent(tablePanel12);
+		//w.addComponent(tablePanel1);
+		
+		buildMainLayout();
+		w.setContent(mainLayout);
 		
 		Window old = starta.getWindow("M");
+		oldURL = old.getURL();
 		old.open(new ExternalResource(w.getURL()));
 	}
 	
 	public void showNotification() {
 		
+	}
+	
+	public void buttonClick (Button.ClickEvent event) {
+		if(event.getButton() == logout){
+			starta.getMainWindow().getApplication().close();
+		}
+		if(event.getButton()==back){
+			w.open(new ExternalResource(oldURL));
+		}
+	}
+	
+	private AbsoluteLayout buildMainLayout() {
+		// common part: create layout
+		mainLayout = new AbsoluteLayout();
+		mainLayout.setImmediate(false);
+		mainLayout.setWidth("100%");
+		mainLayout.setHeight("100%");
+		mainLayout.setMargin(false);
+		
+		// top-level component properties
+		mainLayout.setWidth("100.0%");
+		mainLayout.setHeight("100.0%");
+		
+		// label
+		label = new Label();
+		label.setImmediate(false);
+		label.setWidth("-1px");
+		label.setHeight("-1px");
+		label.setValue("Modulhandbuch organisieren");
+		label.setStyleName(Runo.LABEL_H1);
+		mainLayout.addComponent(label, "top:13.0%;left:35.0%;");
+		
+		// panel_1		
+		mainLayout.addComponent(buildPanel_1(), "top:23.0%;left:35.0%;");
+		
+		// fachname
+		fachname.setCaption("Fachname");
+		fachname.setImmediate(false);
+		fachname.setWidth("30.0%");
+		fachname.setHeight("-1px");
+		mainLayout.addComponent(fachname, "top:49.0%;left:35.0%;");
+		
+		// addFach
+		addFach.setCaption("neues Fach");
+		addFach.setImmediate(false);
+		addFach.setWidth("25.0%");
+		addFach.setHeight("-1px");
+		mainLayout.addComponent(addFach, "top:49.0%;left:54.5%;");
+		
+		// delete
+		delete.setCaption("löschen");
+		delete.setImmediate(false);
+		delete.setWidth("20.0%");
+		delete.setHeight("-1px");
+		mainLayout.addComponent(delete, "top:54.0%;left:35.0%;");
+		
+		// unassignedModules
+		unassignedModules.setCaption("unzugeordnete Module");
+		unassignedModules.setImmediate(false);
+		unassignedModules.setWidth("30.0%");
+		unassignedModules.setHeight("80.0%");
+		mainLayout.addComponent(unassignedModules, "top:61.0%;left:20.0%;");
+		
+		// modules
+		modules.setCaption("zugeordnete Module");
+		modules.setImmediate(false);
+		modules.setWidth("61.0%");
+		modules.setHeight("80.0%");
+		mainLayout.addComponent(modules, "top:61.0%;left:60.6%;");
+		
+		// add1
+		add1.setImmediate(false);
+		add1.setWidth("-1px");
+		add1.setHeight("-1px");
+		mainLayout.addComponent(add1, "top:93.0%;left:20.0%;");
+		
+		// add2
+		add2.setImmediate(false);
+		add2.setWidth("-1px");
+		add2.setHeight("-1px");
+		mainLayout.addComponent(add2, "top:93.0%;left:60.6%;");
+		
+		// logout
+		logout = new Button();
+		logout.setCaption("logout");
+		logout.setImmediate(true);
+		logout.setWidth("-1px");
+		logout.setHeight("-1px");
+		logout.setStyleName(BaseTheme.BUTTON_LINK);
+		logout.addListener(this);
+		mainLayout.addComponent(logout, "top:92.0%;left:5.0%;");
+				
+		// back
+		back = new Button();
+		back.setCaption("Startseite");
+		back.setImmediate(true);
+		back.setWidth("-1px");
+		back.setHeight("-1px");
+		back.setStyleName(BaseTheme.BUTTON_LINK);
+		back.addListener(this);
+		mainLayout.addComponent(back, "top:89.0%;left:5.0%;");
+		
+		return mainLayout;
+	}
+
+	private Panel buildPanel_1() {
+		// common part: create layout
+		pan = new Panel();
+		pan.setCaption("Fächer und Module");
+		pan.setImmediate(false);
+		pan.setWidth("46.0%");
+		pan.setHeight("30.0%");
+		
+		// verticalLayout_1		
+		pan.setContent(buildVerticalLayout_1());
+		
+		return pan;
+	}
+
+	private VerticalLayout buildVerticalLayout_1() {
+		// common part: create layout
+		vl = new VerticalLayout();
+		vl.setImmediate(false);
+		vl.setWidth("100.0%");
+		vl.setHeight("100.0%");
+		vl.setMargin(false);
+		
+		// moduletree
+		moduletree.setImmediate(false);
+		moduletree.setWidth("-1px");
+		moduletree.setHeight("-1px");
+		vl.addComponent(moduletree);
+		
+		return vl;
 	}
 	
 }
